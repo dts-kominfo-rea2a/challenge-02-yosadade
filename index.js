e26f17b9d9df3d46546c816e8606ca0ba2023d1f
yosadade <masyosad@gmail.com>
2022-05-29 20:15:45 +0700

    challenge-02-yosada

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,7 +48,27 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+
+  // menampilkan seluruh data dari arrayObjectPegawai
+  for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+    hasilLooping.push(
+      `${dataYangAkanDilooping[i].namaDepan} ${dataYangAkanDilooping[i].namaBelakang}`
+    );
+    if (dataYangAkanDilooping[i].jenisKelamin === "M") {
+      jumlahPria++;
+    } else if (dataYangAkanDilooping[i].jenisKelamin === "F") {
+      jumlahWanita++;
+    }
+  }
+
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+  } else if (jumlahWanita > jumlahPria) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+  } else if (jumlahPria === jumlahWanita) {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
